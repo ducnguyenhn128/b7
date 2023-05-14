b3753c6027563a2b481a5a3cc110d20e6c3b94e6
ducnguyenhn128 <ducnguyen.dautunhanha@gmail.com>
2023-05-14 19:15:32 +0700

    hbs

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const router = require('./src/controllers/router')
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const bodyParser = require('body-parser');
 const app = express();
 // For handlebars
 const path = require('path');
-const {engine} = requrie('express-handlebars')
+const {engine} = require('express-handlebars')
 
 // Body parser
 app.use(bodyParser.json())
 
 // HandleBars Template Engine
+app.engine('hbs', engine({
+    extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, '/src/views'))
+
+// console.log('PATH: ', path.join(__dirname), 'views')
 
 // API End Point
-app.get('', (req, res) => {
-    res.status(200).send(`Halooooo`)
+app.get('/', (req, res) => {
+    // res.status(200).send(`Halaooooo`)
+    res.render('home')
 })
 
 app.use('/api', router)
